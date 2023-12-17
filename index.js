b6d27fd6364c954f8bde52e7e1dc9c99a8f97144
Huzaifa Jadoon <145706459+huzaifajadoon700@users.noreply.github.com>
2023-12-17 21:55:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.put('/update/:productId', upload.single('productImage'), async (req, res) =>
 });
 //update endsss
 
-app.listen(https://deploy-frontend-eight.vercel.app, () => {
+app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
 
